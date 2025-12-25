5543f8aaaf75d3e8f39440c0ae0c2215ef1b7f3b
Gaurav Chaudhari <chaudharigaurav177@gmail.com>
2025-12-25 21:31:01 +0530

    Added Week 3, Session 12 notebook for model evaluation: included Naive Bayes classifier example and dataset splitting with Scikit-learn.

--- week5_week6/w5and6_s23-s26/Session 23-26/SQL_Content/Basic.sql
+++ week5_week6/w5and6_s23-s26/Session 23-26/SQL_Content/Basic.sql
@@ -1,80 +1,89 @@
 #TABLE RELATED
-select * from emp.employee;
+SELECT * FROM emp.employee;
 
-use emp;
-select * from employee;
+USE emp;
+SELECT * FROM employee;
 
-select emp_id, emp_name from employee;
+SELECT emp_id, emp_name FROM employee;
 
 #WHERE QUERY
-select * from employee where location='Maharashtra';
-select * from employee where job_domain='sales';
-select * from employee where location='maharashtra' and job_domain='sales';
+SELECT * FROM employee WHERE location='Maharashtra';
+SELECT * FROM employee WHERE job_domain='sales';
+SELECT * FROM employee WHERE score = 5;
+SELECT * FROM employee WHERE location='maharashtra' AND job_domain='sales';
 
 #DISTINCT QUERY
-select distinct location from employee;
+SELECT DISTINCT location FROM employee;
+SELECT DISTINCT score FROM employee;
 
 #LIKE QUERY
-select * from employee where emp_name like 's%';
-select * from employee where emp_name like '%ar%';
+SELECT * FROM employee WHERE emp_name LIKE 's%';
+SELECT * FROM employee WHERE emp_name LIKE '%ar%';
+SELECT * FROM  employee WHERE score LIKE '7%';
 
 #AND & BETWEEN
-select * from employee where score>9;
-select * from employee where score>6;
+SELECT * FROM employee WHERE score>9;
+SELECT * FROM employee WHERE score>6;
 
-select * from employee where score>=6 and score<=9;
-select * from employee where score between 6 and 9;
+SELECT * FROM employee WHERE score>=6 AND score<=9;
+SELECT * FROM employee WHERE score BETWEEN 6 AND 9;
+
 
 #OR & IN
-select emp_id, emp_name from employee where joining_date=2000 or joining_date=2001 or joining_date=2005;
-select emp_id, emp_name from employee where joining_date in(2000,2001,2005);
+SELECT emp_id, emp_name FROM employee WHERE joining_date=2000 OR joining_date=2001 OR joining_date=2005;
+SELECT emp_id, emp_name FROM employee WHERE joining_date IN(2000,2001,2005);
+
+SELECT * FROM employee WHERE score BETWEEN 9 AND 10 OR joining_date IN(2001,2002,2003);
 
 #ORDER BY, DESC, LIMIT & OFFSET
-select * from employee where location = 'maharashtra' order by score;
-select * from employee where location = 'maharashtra' order by score desc;
-select * from employee where location = 'maharashtra' order by score desc limit 5;
-select * from employee where location = 'maharashtra' order by score desc limit 5 offset 1;
+SELECT * FROM employee WHERE location = 'maharashtra' ORDER BY score;
+SELECT * FROM employee WHERE location = 'maharashtra' ORDER BY score DESC;
+SELECT * FROM employee WHERE location = 'maharashtra' ORDER BY score DESC LIMIT 5;
+SELECT * FROM employee WHERE location = 'maharashtra' ORDER BY score DESC LIMIT 5 OFFSET 1;     #OFFSET: SKIPS THE FIRST N ROWS
+SELECT * FROM employee WHERE joining_date BETWEEN 1990 AND 2000 ORDER BY score DESC LIMIT 5 OFFSET 3;
 
 #COUNT
-select count(*) from employee where job_domain="marketing";
+SELECT count(*) FROM employee WHERE job_domain="marketing";
+SELECT COUNT(*) FROM employee WHERE score >= 9;
 
 #MAX,MIN,AVG
-select max(score) from employee where job_domain= 'marketing';
-select min(score) from employee where job_domain= 'marketing';
-select avg(score) from employee where job_domain= 'marketing';
-select round(avg(score),1) from employee where job_domain= 'marketing';
+SELECT max(score) FROM employee WHERE job_domain= 'marketing';
+SELECT min(score) FROM employee WHERE job_domain= 'marketing';
+SELECT avg(score) FROM employee WHERE job_domain= 'marketing';
+SELECT round(avg(score),1) FROM employee WHERE job_domain= 'marketing';         #ROUND: ROUNDS THE NUMBER TO 1 DECIMAL PLACE
+SELECT ROUND(AVG(score), 2) FROM employee;
 
-select max(score) as max_score,
+SELECT max(score) as max_score,
 	   min(score) as min_score,
        round(avg(score),1) as avg_score
  from employee 
- where job_domain= 'marketing';
+ WHERE job_domain= 'marketing';
  
 #GROUP BY
- select location, count(location) from employee group by location;
+ SELECT location, count(location) FROM employee group by location;
  
- select job_domain, count(job_domain) as domain_count,round(avg(score),1) as avg_score 
- from employee 
+ SELECT job_domain, count(job_domain) as domain_count,round(avg(score),1) as avg_score
+ FROM employee
  group by job_domain 
  order by avg_score desc;
  
 #HAVING
- select 
+ SELECT
            joining_date,
            count(*) as date_count
-	from employee 
+	FROM employee
 	group by joining_date
 	having date_count>1
 	order by date_count desc;
     
 #IF QUERY   
- select 
+ SELECT
 	emp_name,
 	if (job_domain='sales', 'PROMOTED', 'NEXT TIME PAKKA') as job_status 
-	from employee;
+	FROM employee;
     
 #CASE QUERY    
-    select 
+    SELECT
 	emp_name,
 	case
 		when job_domain='sales' then'PROMOTED'
@@ -82,7 +91,7 @@ select max(score) as max_score,
 		else 'NEXT TIME PAKKA'
 		end
         as job_status
-    from employee;
+    FROM employee;
 
 
 
